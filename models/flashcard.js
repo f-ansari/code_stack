fdc7c492696c6516a5c7d7c1805124f999f9b03d
Luke Arenas <lukedarenas@gmail.com>
2021-03-22 09:36:02 -0600

    created deck and flashcard models

---
+++ models/flashcard.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Flashcard extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Flashcard.init({
+    title: DataTypes.STRING,
+    notes: DataTypes.STRING,
+    codeBlock: DataTypes.STRING,
+    deckId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Flashcard',
+  });
+  return Flashcard;
+};
