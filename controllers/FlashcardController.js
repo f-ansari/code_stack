3faf26041a79cdcadcac5657e3feed3b19654d8a
Luke Arenas <lukedarenas@gmail.com>
2021-03-22 11:19:28 -0600

    set up functions in Flashcard Controller

--- controllers/FlashcardController.js
+++ controllers/FlashcardController.js
@@ -1,3 +1,24 @@
-const {Flashcards}=require('../models')
-const { op }= require('sequelize')
-const flashcards = require('./model')
+const { Flashcard } = require('../models')
+const { op } = require('sequelize')
+
+getFlashcardsByDeck = async (req, res) => {
+  try {
+  } catch (error) {}
+}
+
+updateFlashcard = async (req, res) => {
+  try {
+  } catch (error) {}
+}
+
+deleteFlashcard = async (req, res) => {
+  try {
+  } catch (error) {}
+}
+
+createNewFlashcard = async (req, res) => {
+  try {
+  } catch (error) {}
+}
+
+module.exports = {}
