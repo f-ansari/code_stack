0440b53c1eab579af9632c203a2c9ea259dd18dc
Luke Arenas <lukedarenas@gmail.com>
2021-03-22 17:34:57 -0600

    added authRouter

--- middleware/index.js
+++ middleware/index.js
@@ -1,52 +1,52 @@
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
-const SALT_ROUNDS = 12
-const SECRET_KEY = 'supersecret'
+const SALT_ROUNDS = process.env.SALT_ROUNDS
+const SECRET_KEY = process.env.SECRET_KEY
 
 const HashPassword = async (password) => {
-const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS)
-return hashedPassword
+  const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS)
+  return hashedPassword
 }
 
 const ComparePassword = async (providedPassword, storedPassword) => {
-return await bcrypt.compare(providedPassword, storedPassword)
+  return await bcrypt.compare(providedPassword, storedPassword)
 }
 
-const StripHeaders = (req, res, next) => {
-try {
+const StripToken = (req, res, next) => {
+  try {
     const token = req.headers.authorization.split(' ')[1]
     if (token) {
-    res.locals.token = token
-    return next()
+      res.locals.token = token
+      return next()
     }
     return res.status(401).json({ msg: 'Unauthorized' })
-} catch (error) {
+  } catch (error) {
     console.log(error)
     res.status(401).json({ msg: 'Unauthorized' })
-}
+  }
 }
 
 const VerifyToken = (req, res, next) => {
-const { token } = res.locals
-try {
+  const { token } = res.locals
+  try {
     let payload = jwt.verify(token, SECRET_KEY)
     res.locals.token = payload
     return next()
-} catch (error) {
+  } catch (error) {
     return res.status(401).json({ msg: 'Unauthorized' })
-}
+  }
 }
 
 const CreateToken = (payload) => {
-let token = jwt.sign(payload, SECRET_KEY)
-return token
+  let token = jwt.sign(payload, SECRET_KEY)
+  return token
 }
 
 module.exports = {
-HashPassword,
-ComparePassword,
-StripHeaders,
-VerifyToken,
-CreateToken
+  HashPassword,
+  ComparePassword,
+  StripToken,
+  VerifyToken,
+  CreateToken
 }
