20ff8be6348d5aac3671c20a5b63510ac4bfa37c
nickwill24 <nick2428.nw@gmail.com>
2021-03-22 12:48:11 -0400

    add middleware file

--- models/user.js
+++ models/user.js
@@ -9,8 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      User.hasMany(models.Deck, { as: 'decks', foreignKey: 'userId'})
-      User.hasMany(models.FlashCard, { as: 'flashcards', foreignKey: 'userId'})
+      User.hasMany(models.Deck, { foreignKey: 'userId'})
     }
   }
   User.init(
