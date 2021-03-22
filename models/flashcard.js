8a433eb95089436fc0cd09ab93a50ed3d100660a
Luke Arenas <lukedarenas@gmail.com>
2021-03-22 09:47:20 -0600

    set up associations

--- models/flashcard.js
+++ models/flashcard.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Flashcard.belongsTo(models.Deck, { foreignKey: 'deckId' })
     }
   }
   Flashcard.init(
--- models/user.js
+++ models/user.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+'use strict'
+const { Model } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -10,20 +8,22 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
-      
+      User.hasMany(models.Deck, { foreignKey: 'userId' })
     }
-  };
-  User.init({
-    handle: DataTypes.STRING,
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    passwordDigest: DataTypes.STRING,
-    avatarUrl: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'User',
-    tableName: 'users'
-  });
-  return User;
-};
+  }
+  User.init(
+    {
+      handle: DataTypes.STRING,
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      passwordDigest: DataTypes.STRING,
+      avatarUrl: DataTypes.STRING
+    },
+    {
+      sequelize,
+      modelName: 'User',
+      tableName: 'users'
+    }
+  )
+  return User
+}
