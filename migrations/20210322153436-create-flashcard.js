196255ecf66d447f20223d590a560fdc5b1b5301
Luke Arenas <lukedarenas@gmail.com>
2021-03-22 11:12:31 -0600

    adjusted drop tables on migrations

--- migrations/20210322153436-create-flashcard.js
+++ migrations/20210322153436-create-flashcard.js
@@ -31,6 +31,6 @@ module.exports = {
     })
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Flashcards')
+    await queryInterface.dropTable('flashcards')
   }
 }
