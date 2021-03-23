deb6211ab21d6a12f35e6c259e668518a8619b4f
Faryal Ansari <faryalansari@live.com>
2021-03-22 20:32:46 -0400

    login form complete w/o submit f()

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,8 +1,33 @@
 import React, { useState } from 'react'
 
 const LoginForm = () => {
+  const [handle, setHandle] = useState('')
+  const [password, setPassword] = useState('')
+
   return (
-    <div>LoginForm</div>
+    <div>
+      <h1>Login Form</h1>
+
+      <form>
+        <input 
+          type = "text"
+          name = "handle"
+          value = {handle}
+          onChange = {(e) => setHandle(e.target.value)}
+          placeholder="jane_doe"
+        />
+        <input
+          type = "text"
+          name = "password"
+          value = {password}
+          onChange = {(e) => setPassword(e.target.value)}
+          placeholder="jane_doe"
+        />
+        <button color="black" type="submit">
+            Submit
+          </button>
+      </form>
+    </div>
   )
 }
 
