632d9e20d02be7e49fdf0af8c516a21b09798ccb
Luke Arenas <lukedarenas@gmail.com>
2021-03-23 08:49:03 -0600

    migrated language column to flashcards

--- migrations/20210323143134-add-language-to-flashcards.js
+++ migrations/20210323143134-add-language-to-flashcards.js
@@ -1,21 +1,13 @@
-'use strict';
+'use strict'
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.addColumn('flashcards', 'language', {
+      type: Sequelize.STRING
+    })
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.removeColumn('flashcards', 'language')
   }
-};
+}
