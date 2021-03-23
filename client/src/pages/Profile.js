2a9bb7a3fa03424c85b6a1514b2f379982cfba32
Christina <christinapadilla.cp@gmail.com>
2021-03-23 11:12:20 -0500

    set up Profile to receive and display props

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,7 +1,72 @@
 import React, { useState, useEffect } from 'react'
+import DeckForm from '../components/CreateDeck'
+import { SET_SELECTED_DECK, SET_SELECTED_USER } from '../store/types'
+// import { BASE_URL } from '../globals'
+// import axios from 'axios'
 
-const Profile = () => {
-  return <h1>Profile</h1>
+const Profile = (props) => {
+  const {
+    currentUser,
+    selectedUser,
+    selectedDeck,
+    decksByHandle,
+    dispatch
+  } = props
+
+  const getProfile = async () => {
+    // fetch profile from db using selectedUser.id or selectedUser.handle??
+    // TODO: to fetch decks with user, we would need to update the getProfile controller
+    try {
+      //const res = await axios.get(`${BASE_URL}/${props.match.params.handle}`)
+      // dispatch({ type: SET_SELECTED_USER, payload: res.data })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const switchButton = () => {
+    // SWITCH statement
+    // render createDeck btn if selectedUser matches currentUser
+    // render follow btn if selectedUser does not match currentUser & checkFollowing is false
+    // render unfollow btn is selectedUser does not match currentUser & checkFollowing is true
+    switch (true) {
+      case currentUser && currentUser.handle:
+        return <button>+ Create Deck</button>
+      default:
+        return <button>Follow</button>
+    }
+  }
+
+  // map through all of the decks owned by selectedUser
+  const renderDecksByHandle = () => {
+    return decksByHandle.map((deck, idx) => (
+      <div key={`${idx}`} onClick={() => targetDeck(deck.id)}>
+        <h3>Deck Title: {deck.title}</h3>
+      </div>
+    ))
+  }
+
+  // route user to deck page to view deck details
+  const targetDeck = (deckId) => {
+    dispatch({ type: SET_SELECTED_DECK, payload: deckId })
+    props.params.history.push(`/deck/${deckId}`)
+  }
+
+  useEffect(() => {
+    getProfile()
+  }, [props.match.params.handle])
+
+  return (
+    <div>
+      <h1>Profile</h1>
+      {switchButton}
+      <img
+        src={currentUser ? currentUser.avatarUrl : null}
+        alt={`avatar for ${currentUser ? currentUser.handle : null}`}
+      />
+      <div>{renderDecksByHandle}</div>
+    </div>
+  )
 }
 
 export default Profile
