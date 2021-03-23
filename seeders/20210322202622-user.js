892ab058fd5c203e70f6eb39a431c1a75418e05d
Luke Arenas <lukedarenas@gmail.com>
2021-03-23 09:44:29 -0600

    revised users seed with faker data

--- seeders/20210322202622-user.js
+++ seeders/20210322202622-user.js
@@ -1,18 +1,19 @@
 const faker = require('faker')
 const bcrypt = require('bcrypt')
 
-const users = [...Array(10)].map(() => ({
-  name: faker.name.firstName(),
-  email: faker.internet.email(),
-  handle: faker.internet.userName(),
-  passwordDigest: '',
-  avatarUrl: faker.internet.avatar(),
-  createdAt: faker.date.past(),
-  updatedAt: faker.date.recent(15)
-}))
-
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    const users = await Promise.all(
+      [...Array(10)].map(async () => ({
+        name: faker.name.firstName(),
+        email: faker.internet.email(),
+        handle: faker.internet.userName(),
+        passwordDigest: await bcrypt.hash('1234', 1),
+        avatarUrl: faker.internet.avatar(),
+        createdAt: faker.date.past(),
+        updatedAt: faker.date.recent(15)
+      }))
+    )
     await queryInterface.bulkInsert('users', users)
   },
 
