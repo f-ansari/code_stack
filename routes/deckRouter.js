c64dde0cc1198f07d8511048115a2d9697387b44
nickwill24 <nick2428.nw@gmail.com>
2021-03-23 09:30:11 -0400

    add the get all deck and route

--- routes/deckRouter.js
+++ routes/deckRouter.js
@@ -4,6 +4,7 @@ const controller = require('../controllers/DeckController')
 
 
 Router.post('/:user_Id', controller.createDecks)
+Router.get('/view', controller.getAllDecks)
 Router.get('/view/:deck_Id', controller.getOneDecks)
 Router.put('/:deck_Id', controller.updateDecks)
 Router.delete('/:deck_Id', controller.deleteDecks)
