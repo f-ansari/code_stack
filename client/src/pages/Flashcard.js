4a1ab7657038f36e4440799b770fb1fc0769ff24
nickwill24 <nick2428.nw@gmail.com>
2021-03-23 16:46:33 -0400

    update flashcard page

--- client/src/pages/Flashcard.js
+++ client/src/pages/Flashcard.js
@@ -1,7 +1,41 @@
 import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { BASE_URL } from '../globals'
 
 const Flashcard = () => {
-  return <h1>Flashcard</h1>
-}
+  // const[notes, setnotes]=useState('')
+  // const[language, setlanguge] =useState('')
+  // const[codeBlock, setCodeBlock]= useState('')
+  // const[title, settitle]= useState('')
+  const [getOneCard, setGetOneCard]=useState('')
+
+  const getOneCard= async () =>{
+    try{
+      const res = await axios.get(`${BASE_URL}/flashcard/one/${deckId}`)
+      setGetOneCard(res.data)
+    } catch (error){
+      console.log(error)
+    }
+  }
+
+useEffect(()=>{
+
+},[])
+
+
+
+
+
+
+
+  return(
+    <div>
+      <h1>{props.title}</h1>
+      <p>{props.codeBlock}</p>
+      <p>{props.noted}</p>
+      <p>{props.language}</p>
+      <button>Back to Flashcard</button>
+    </div>
+  )}
 
 export default Flashcard
