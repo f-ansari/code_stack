265c216c65d34d4861398fb8743d0b2d77db634c
Faryal Ansari <77459938+f-ansari@users.noreply.github.com>
2021-03-23 17:17:28 -0400

    Revert "Nick branch"

--- client/src/pages/Flashcard.js
+++ client/src/pages/Flashcard.js
@@ -1,36 +1,7 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import { BASE_URL } from '../globals'
 
-const Flashcard = (props) => {
-  const [getOneCard, setGetOneCard]=useState('')
-  const { flashcardId }= props.match.params
-
-  const getOneCard= async () =>{
-    try{
-      const res = await axios.get(`${BASE_URL}/flashcards/${flashcardId}`)
-      setGetOneCard(res.data)
-    } catch (error){
-      console.log(error)
-    }
-  }
-
-useEffect(()=>{
-getOneCard()
-},[flashcardId])
-
-
-
-
-
-  return(
-    <div>
-      <h1>{props.title}</h1>
-      <p>{props.codeBlock}</p>
-      <p>{props.notes}</p>
-      <p>{props.language}</p>
-      <button>Back to Deck</button>
-    </div>
-  )}
+const Flashcard = () => {
+  return <h1>Flashcard</h1>
+}
 
 export default Flashcard
