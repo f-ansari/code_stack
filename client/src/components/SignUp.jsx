8d9c1c41e2c91b800f6b131be8a434d82d5c0aa9
Christina <christinapadilla.cp@gmail.com>
2021-03-23 07:52:17 -0500

    add conditional rend. to handle signup to login transition

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,5 +1,5 @@
-import React, { useReducer, useEffect } from 'react'
-import { useHistory } from 'react-router-dom'
+import React, { useReducer } from 'react'
+import { NavLink } from 'react-router-dom'
 import {
   // TODO add these types of store
   SIGNUP_FORM,
@@ -7,6 +7,8 @@ import {
 } from '../store/types'
 import { BASE_URL } from '../globals'
 import axios from 'axios'
+
+// TODO:  add props.currentUser to PubNav component so that handle can be pre-filled in when user is routed to login post-register
 
 const iState = {
   register: {
@@ -39,7 +41,6 @@ const reducer = (state, action) => {
 
 const SignUp = () => {
   const [state, dispatch] = useReducer(reducer, iState)
-  const history = useHistory()
 
   const handleChange = (event) => {
     dispatch({
@@ -51,8 +52,8 @@ const SignUp = () => {
   const handleSubmit = async (e) => {
     try {
       await axios.post(`${BASE_URL}/auth/register`, state.register)
-
       dispatch({ type: SUBMIT_SIGNUP, payload: true })
+      // history.push('/login') // ! should we route user to login page once register form is submitted. Confirmation Message?
     } catch (err) {
       console.log(err)
     }
@@ -60,46 +61,57 @@ const SignUp = () => {
 
   return (
     <div>
-      <h1>SignUp</h1>
-      <form onSubmit={(event) => handleSubmit(event)}>
-        <input
-          name="firstname"
-          placeholder="First Name"
-          type="text"
-          value={state.firstname}
-          onChange={(event) => handleChange(event)}
-        />
-        <input
-          name="handle"
-          placeholder="handle"
-          type="text"
-          value={state.handle}
-          onChange={(event) => handleChange(event)}
-        />
-        <input
-          name="email"
-          placeholder="email@me.com"
-          type="text"
-          value={state.email}
-          onChange={(event) => handleChange(event)}
-        />
-        <input
-          name="password"
-          placeholder="password"
-          type="text"
-          value={state.password}
-          onChange={(event) => handleChange(event)}
-        />
-        <input
-          name="avatar"
-          placeholder="Add an image URL to use for your profile"
-          type="avatar"
-          value={state.avatar}
-          onChange={(event) => handleChange(event)}
-        />
+      {state.signupSubmitted ? (
+        <div>
+          <h1>SignUp</h1>
+          <form onSubmit={(event) => handleSubmit(event)}>
+            <input
+              name="firstname"
+              placeholder="First Name"
+              type="text"
+              value={state.firstname}
+              onChange={(event) => handleChange(event)}
+            />
+            <input
+              name="handle"
+              placeholder="handle"
+              type="text"
+              value={state.handle}
+              onChange={(event) => handleChange(event)}
+            />
+            <input
+              name="email"
+              placeholder="email@me.com"
+              type="text"
+              value={state.email}
+              onChange={(event) => handleChange(event)}
+            />
+            <input
+              name="password"
+              placeholder="password"
+              type="text"
+              value={state.password}
+              onChange={(event) => handleChange(event)}
+            />
+            <input
+              name="avatar"
+              placeholder="Add an image URL to use for your profile"
+              type="avatar"
+              value={state.avatar}
+              onChange={(event) => handleChange(event)}
+            />
 
-        <input type="submit" value="Submit" />
-      </form>
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
+      ) : (
+        <div>
+          <h2>Nice! Your account was created.</h2>
+          <NavLink to="/login">
+            <button>Sign in</button>
+          </NavLink>
+        </div>
+      )}
     </div>
   )
 }
