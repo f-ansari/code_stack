8184c73eb81389e44854fdfcd724481eb0993fca
Christina <christinapadilla.cp@gmail.com>
2021-03-25 09:31:41 -0500

    add console log to createflashcard

--- client/src/components/CreateFlashcard.jsx
+++ client/src/components/CreateFlashcard.jsx
@@ -48,6 +48,7 @@ const reducer = (state, action) => {
 
 const CreateFlashcard = (props) => {
   const [state, dispatch] = useReducer(reducer, iState)
+  console.log(props)
 
   const handleFlashcardSubmit = async (e) => {
     e.preventDefault()
