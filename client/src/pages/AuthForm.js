b9f3846bc6b0d431a9eda97f4f4c621868a2bc7d
Faryal Ansari <faryalansari@live.com>
2021-03-25 16:18:03 -0400

    setup for initial deployment

--- client/src/pages/AuthForm.js
+++ client/src/pages/AuthForm.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 
 const AuthForm = () => {
-  return <h1>AuthForm</h1>
+  return <h1>AuthForm Just here for deployment</h1>
 }
 
 export default AuthForm
