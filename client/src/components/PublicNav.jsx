cbd4335d6a2a6175c3b5d1c3113109f56b07b67a
Faryal Ansari <faryalansari@live.com>
2021-03-25 17:48:26 -0400

    added home button

--- client/src/components/PublicNav.jsx
+++ client/src/components/PublicNav.jsx
@@ -4,6 +4,9 @@ import { NavLink } from 'react-router-dom'
 const PublicNav = () => {
   return (
     <div>
+      <NavLink to="/">
+        <button className="nav-buttons">HOME</button>
+      </NavLink>
       <NavLink to="/login">
         <button className="nav-buttons">Login</button>
       </NavLink>
